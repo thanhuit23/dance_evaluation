c743c6def4618892cf82a089b2343c801630e79a
thanhit08Tekville <thanhit08@tekville.com>
2024-05-26 19:18:16 +0900

    extract_frames mediapipe_pose_estimation_frame mediapipe_pose_estimation_frame_numpy mediapipe_pose_estimation_consistency normalize_keypoints padding_keypoints_to_sequence

--- libs/oneDataCollection.py
+++ libs/oneDataCollection.py
@@ -2,8 +2,15 @@
 # Data Collection (Collect data from various sources like databases, APIs, files, etc.)
 import cv2
 import os
+import mediapipe as mp
+import numpy as np
 
-# Function to convert video resolution to a specific width and height 
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+
+# Function to convert video resolution to a specific width and height
+
+
 def convert_video_resolution(input_path, output_path, width=640, height=480):
     try:
         # Check if the input file exists
@@ -47,7 +54,8 @@ def convert_video_resolution(input_path, output_path, width=640, height=480):
 
         # Define the codec and create VideoWriter object
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(output_path, fourcc, original_fps, (width, height))
+        out = cv2.VideoWriter(output_path, fourcc,
+                              original_fps, (width, height))
 
         frame_count = 0
         while cap.isOpened():
@@ -59,8 +67,8 @@ def convert_video_resolution(input_path, output_path, width=640, height=480):
             resized_frame = cv2.resize(frame, (new_width, new_height))
             # Add black bars to the sides or top and bottom of the frame
             padded_frame = cv2.copyMakeBorder(resized_frame, vertical_padding, vertical_padding,
-                                                horizontal_padding, horizontal_padding,
-                                                cv2.BORDER_CONSTANT, value=(0, 0, 0))
+                                              horizontal_padding, horizontal_padding,
+                                              cv2.BORDER_CONSTANT, value=(0, 0, 0))
 
             if frame_count == 0:
                 # Save padding frame for debugging
@@ -78,12 +86,12 @@ def convert_video_resolution(input_path, output_path, width=640, height=480):
 
     except Exception as e:
         print(f"An error occurred: {e}")
-        
+
+
 def convert_video_fps(input_path, output_path, fps=30):
     # Using ffmpeg to convert the video to 30 fps
     os.system(f'ffmpeg -i "{input_path}" -r {fps} "{output_path}"')
-    print(f"Video saved to {output_path}")    
-    
+    print(f"Video saved to {output_path}")
 
 
 # Split the video into 20-second clips based on the music timestamps
@@ -109,7 +117,6 @@ def split_video(input_path, output_dir, clip_duration=20):
         # Get the width and height of the video
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
 
         # Calculate the duration of each frame
         original_frame_duration = 1 / original_fps
@@ -120,6 +127,8 @@ def split_video(input_path, output_dir, clip_duration=20):
         # Calculate the number of clips based on the clip duration
         num_clips = int(video_duration // clip_duration)
 
+        print(f"Total frames: {total_frames}")
+
         # Create the output directory if it does not exist
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -129,7 +138,7 @@ def split_video(input_path, output_dir, clip_duration=20):
             start_time = i * clip_duration
             end_time = (i + 1) * clip_duration
             if end_time > video_duration:
-                break # Skip the last clip if it exceeds the video duration
+                break  # Skip the last clip if it exceeds the video duration
 
             # Set the start frame number based on the start time
             start_frame = int(start_time / original_frame_duration)
@@ -138,12 +147,14 @@ def split_video(input_path, output_dir, clip_duration=20):
             end_frame = int(end_time / original_frame_duration)
 
             # Set the output file path
-            output_filename = os.path.basename(input_path).replace(".mp4", f"_clip_{i}.mp4")
+            output_filename = os.path.basename(
+                input_path).replace(".mp4", f"_clip_{i}.mp4")
             output_path = os.path.join(output_dir, output_filename)
 
             # Define the codec and create VideoWriter object
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            out = cv2.VideoWriter(output_path, fourcc, original_fps, (width, height))
+            out = cv2.VideoWriter(output_path, fourcc,
+                                  original_fps, (width, height))
 
             # Set the frame count to 0
             frame_count = 0
@@ -164,14 +175,20 @@ def split_video(input_path, output_dir, clip_duration=20):
             out.release()
 
             print(f"Clip {i} saved to {output_path}")
+
+            if i == len(range(num_clips)) - 1:
+                print(f"Total frames processed: {frame_count}")
+
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         # Release the input video
         cap.release()
-        
+
 # Fix the video lighting and color balance
 # Labeling the video clips based on "good choreography" or "bad choreography"
+
+
 def create_label_file(input_dir, output_file):
     try:
         # Open the output file in write mode
@@ -190,18 +207,235 @@ def create_label_file(input_dir, output_file):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+# Extract frame from videos
+
+
+def extract_frames(input_video, output_dir, frame_rate=1):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    cap = cv2.VideoCapture(input_video)
+    frame_count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frame_count % frame_rate == 0:
+            # format the frame count to have 2 digits
+            frame_count_str = str(frame_count).zfill(2)
+            frame_filename = os.path.join(
+                output_dir, f"frame_{frame_count_str}.jpg")
+            cv2.imwrite(frame_filename, frame)
+        frame_count += 1
+    cap.release()
+    print(f"Frames extracted to {output_dir}")
+    print(f"Total frames extracted: {frame_count}")
+
+# Extract pose information from frame image
+
+
+def extract_pose_information(frame_image):
+    # Implement pose estimation using OpenPose or other libraries
+    pass
+
 # Pose estimation using OpenPose or other libraries
+
+
 def open_pose_estimation(input_video, output_dir):
     # Implement pose estimation using OpenPose or other libraries
     pass
 
-def mediapipe_pose_estimation(input_video, output_dir):
-    # Implement pose estimation using MediaPipe or other libraries
-    pass
+
+def mediapipe_pose_estimation_frame(input_frame, output_dir):
+
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+
+    # Convert the frame to numpy array
+    input_frame = cv2.imread(input_frame)
+
+    # Convert the frame to RGB
+    frame_rgb = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with MediaPipe Pose
+    results = pose.process(frame_rgb)
+
+    # Draw the pose landmarks on the frame
+    annotated_frame = input_frame.copy()
+    mp.solutions.drawing_utils.draw_landmarks(
+        annotated_frame, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+    # Save the annotated frame to the output directory
+    output_path = os.path.join(output_dir, "annotated_frame.jpg")
+    cv2.imwrite(output_path, annotated_frame)
+
+    # Save the pose landmarks to a csv file
+    pose_csv_path = os.path.join(output_dir, "pose_landmarks.csv")
+    with open(pose_csv_path, 'w') as pose_file:
+        for landmark in results.pose_landmarks.landmark:
+            pose_file.write(f"{landmark.x},{landmark.y},{landmark.z}\n")
+    pose.close()
+
+    print(f"Annotated frame saved to {output_path}")
+
+
+def mediapipe_pose_estimation_frame_numpy(input_frame, output_dir):
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+
+    # Convert the frame to numpy array
+    input_frame_mat = cv2.imread(input_frame)
+
+    # Convert the frame to RGB
+    frame_rgb = cv2.cvtColor(input_frame_mat, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with MediaPipe Pose
+    results = pose.process(frame_rgb)
+
+    # Draw the pose landmarks on the frame
+    annotated_frame = input_frame_mat.copy()
+    frame_keypoints = []
+    frame_confidences = []
+    if results.pose_landmarks is None:
+        print("No pose landmarks detected in the frame.")
+        error_estimation_folder = os.path.join(os.getcwd(), "test_videos", "error_estimation")
+        if not os.path.exists(error_estimation_folder):
+            os.makedirs(error_estimation_folder)
+        # Save the image to "error_estimation" folder
+        cv2.imwrite(os.path.join(error_estimation_folder, os.path.basename(input_frame)), input_frame_mat)
+        pose.close()
+        return False
+
+    for lm in results.pose_landmarks.landmark:
+        frame_keypoints.append([lm.x, lm.y, lm.z])
+        frame_confidences.append(lm.visibility)
+    mp.solutions.drawing_utils.draw_landmarks(
+        annotated_frame, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+    # Save the annotated frame to the output directory
+    output_path = os.path.join(output_dir, "annotated_frame.jpg")
+    cv2.imwrite(output_path, annotated_frame)
+
+    # Save the pose landmarks to a csv file
+    pose_csv_path = os.path.join(output_dir, "pose_landmarks.csv")
+    with open(pose_csv_path, 'w') as pose_file:
+        for landmark in results.pose_landmarks.landmark:
+            pose_file.write(f"{landmark.x},{landmark.y},{landmark.z}\n")
+
+    pose_numpy_path = os.path.join(output_dir, "pose_numpy.npy")
+    np.save(pose_numpy_path, np.array(frame_keypoints))
+    conf_numpy_path = os.path.join(output_dir, "conf_numpy.npy")
+    np.save(conf_numpy_path, np.array(frame_confidences))
+
+    pose.close()
+
+    print(f"Annotated frame saved to {output_path}")
+    return True
+
+
+def mediapipe_pose_estimation_consistency(frames_folder, threshold=1.5):
+    list_of_frames = os.listdir(frames_folder)
+    num_frames = len(list_of_frames)
+
+    key_points = []
+    for frame in range(num_frames):
+        frame_str = f"frame_{str(frame).zfill(2)}"
+        frame_path = os.path.join(frames_folder, frame_str)
+        frame_pose_file = os.path.join(frame_path, "pose_numpy.npy")
+
+        if not os.path.exists(frame_pose_file):
+            print(f"Error: Pose landmarks file {
+                  frame_pose_file} does not exist.")
+            key_points.append(None)
+            continue
+
+        frame_keypoints = np.load(frame_pose_file)
+        key_points.append(frame_keypoints)
+
+    for i in range(1, num_frames):
+        if key_points[i] is None:
+            continue
+        # check the consistency of the number of keypoints detected in each frame using np linalog norm
+        frame_diff = np.linalg.norm(key_points[i] - key_points[i-1])
+
+        if np.any(frame_diff > threshold):
+            print(f"Inconsistent keypoints detected between frame {i-1} and {i}")
+            current_keypoints = key_points[i]
+            print(f"Key points in frame {i-1}: {key_points[i-1]}")
+            # convert numpy array to list
+            current_keypoints_list = current_keypoints.tolist()
+            previous_keypoints = key_points[i-1]
+            print(f"Key points in frame {i}: {key_points[i]}")
+            previous_keypoints_list = previous_keypoints.tolist()
+            current_frame_name = f"frame_{str(i).zfill(2)}"
+            previous_frame_name = f"frame_{str(i - 1).zfill(2)}"
+            # extracted frames folder
+            parent_folder = os.path.dirname(frames_folder)
+            extracted_frames_folder = os.path.join(
+                parent_folder, "extracted_frames")
+            current_frame_file = os.path.join(
+                extracted_frames_folder, f"{current_frame_name}.jpg")
+            current_frame_math = cv2.imread(current_frame_file)
+            for current_keypoint, previous_keypoint in zip(current_keypoints_list, previous_keypoints_list):
+                x, y, _ = current_keypoint
+                # convert the mediapipe keypoint to opencv keypoint
+                image_height, image_width, _ = current_frame_math.shape
+                x = int(x * image_width)
+                y = int(y * image_height)
+                cv2.circle(current_frame_math, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+                x, y, _ = previous_keypoint
+                x = int(x * image_width)
+                y = int(y * image_height)
+                cv2.circle(current_frame_math, (int(x), int(y)), 5, (0, 255, 0), -1)
+            # round the frame diff to 2 decimal places
+            frame_diff = round(frame_diff, 2)
+            # draw score on the frame
+            cv2.putText(current_frame_math, f"Diff: {frame_diff}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            error_frame_path = os.path.join(parent_folder, "pose_error")
+            if not os.path.exists(error_frame_path):
+                os.makedirs(error_frame_path)
+            frame_error_path = os.path.join(error_frame_path, f"{current_frame_name}_{previous_frame_name}.jpg")
+            cv2.imwrite(frame_error_path, current_frame_math)
+            return False
+    print("Consistent keypoints detected in all frames")
+    return True
+
+
+def normalize_keypoints(keypoints):
+    min_val = np.min(keypoints)
+    max_val = np.max(keypoints)
+    normalized_keypoints = (keypoints - min_val) / (max_val - min_val)
+    return normalized_keypoints
+
+def padding_keypoints_to_sequence(frames_numpy_folder, max_sequence_length):
+    list_frame_folders = os.listdir(frames_numpy_folder)
+    padding_keypoints = []
+    sequence_keypoints = []
+    sequence_folder = os.path.join(os.path.dirname(frames_numpy_folder), "mediapipe_pose_estimation_sequence")
+    if not os.path.exists(sequence_folder):
+        os.makedirs(sequence_folder)
+        
+    for i in range(len(list_frame_folders)):
+        if i == 1199:
+            print("debug")
+        # copy each max_sequence_length keypoints to padding_keypoints
+        folder_name = f"frame_{str(i).zfill(2)}"
+        keypoints = np.load(os.path.join(frames_numpy_folder, folder_name, "pose_numpy.npy"))
+        sequence_keypoints.append(keypoints)
+        if i != 0 and i % (max_sequence_length - 1) == 0:
+            padding_keypoints.append(sequence_keypoints)
+            sequence_keypoints_numpy = np.array(sequence_keypoints)
+            np.save(os.path.join(sequence_folder, f"{folder_name}.npy"), sequence_keypoints_numpy)
+            sequence_keypoints = []
+    
+    # save the padding keypoints to numpy file
+    # padding_keypoints = np.array(padding_keypoints)
+    # np.save(os.path.join(os.path.dirname(frames_numpy_folder), "padding_keypoints.npy"), padding_keypoints)
+    return padding_keypoints
+
 
 def yolo_v8_pose_l_pose_estimation(input_video, output_dir):
     # Implement pose estimation using YOLO V8 Pose L
     pass
+
 
 def yolo_nas_pose_m_pose_estimation(input_video, output_dir):
     # Implement pose estimation using YOLO NAS Pose M
--- tests/one_datacollection_test.py
+++ tests/one_datacollection_test.py
@@ -1,14 +1,17 @@
+from libs import oneDataCollection as oneDataCollection
 import cv2
 import os
 import sys
+import numpy as np
 # import unittest   # The test framework
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # import 1-data-collection from libs
-from libs import oneDataCollection as oneDataCollection
 
 # class TestDataCollection(unittest.TestCase):
+
+
 def test_convert_video_resolution():
     width = 640
     height = 480
@@ -16,37 +19,41 @@ def test_convert_video_resolution():
     video_path = os.path.join(os.getcwd(), "test_videos", "landscape.mp4")
     new_video_path = video_path.replace(".mp4", "_resolution_converted.mp4")
     # Print current working directory
-    oneDataCollection.convert_video_resolution(video_path, new_video_path, width, height)
+    oneDataCollection.convert_video_resolution(
+        video_path, new_video_path, width, height)
     assert os.path.exists(new_video_path)
     video = cv2.VideoCapture(new_video_path)
     assert video.get(cv2.CAP_PROP_FRAME_WIDTH) == width
     assert video.get(cv2.CAP_PROP_FRAME_HEIGHT) == height
-    
+
     video.release()
     os.remove(new_video_path)
 
     # Test case 2: Video in portrait mode
     video_path = os.path.join(os.getcwd(), "test_videos", "portrait.mp4")
     new_video_path = video_path.replace(".mp4", "_resolution_converted.mp4")
-    oneDataCollection.convert_video_resolution(video_path, new_video_path, width, height)
+    oneDataCollection.convert_video_resolution(
+        video_path, new_video_path, width, height)
     assert os.path.exists(new_video_path)
     video = cv2.VideoCapture(new_video_path)
     assert video.get(cv2.CAP_PROP_FRAME_WIDTH) == width
-    assert video.get(cv2.CAP_PROP_FRAME_HEIGHT) == height    
+    assert video.get(cv2.CAP_PROP_FRAME_HEIGHT) == height
     video.release()
     os.remove(new_video_path)
 
     # Test case 3: Video with equal width and height
     video_path = os.path.join(os.getcwd(), "test_videos", "square.mp4")
     new_video_path = video_path.replace(".mp4", "_resolution_converted.mp4")
-    oneDataCollection.convert_video_resolution(video_path, new_video_path, width, height)
-    assert os.path.exists(new_video_path)    
+    oneDataCollection.convert_video_resolution(
+        video_path, new_video_path, width, height)
+    assert os.path.exists(new_video_path)
     video = cv2.VideoCapture(new_video_path)
     assert video.get(cv2.CAP_PROP_FRAME_WIDTH) == width
     assert video.get(cv2.CAP_PROP_FRAME_HEIGHT) == height
     video.release()
     os.remove(new_video_path)
-        
+
+
 def test_convert_video_fps():
     # Test converting a video to 30 fps
     input_path = os.path.join(os.getcwd(), "test_videos", "landscape.mp4")
@@ -65,15 +72,16 @@ def test_convert_video_fps():
     cap.release()
     os.remove(output_path)
     assert converted_fps == fps
-        
+
+
 def test_split_video():
     input_video = os.path.join(os.getcwd(), "test_videos", "test_split.mp4")
     output_dir = os.path.join(os.getcwd(), "test_videos", "split_clips")
     clip_duration = 20
-    
+
     cap = cv2.VideoCapture(input_video)
     # Get the frame rate of the input video
-    original_fps = cap.get(cv2.CAP_PROP_FPS)    
+    original_fps = cap.get(cv2.CAP_PROP_FPS)
     # Calculate the total number of frames in the video
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     original_frame_duration = 1 / original_fps
@@ -81,7 +89,7 @@ def test_split_video():
     video_duration = total_frames * original_frame_duration
     # Calculate the number of clips based on the clip duration
     expected_num_clips = int(video_duration // clip_duration)
-    
+
     # Call the split_video function
     oneDataCollection.split_video(input_video, output_dir, clip_duration)
 
@@ -91,30 +99,167 @@ def test_split_video():
 
     # Check if the clips have the correct duration
     for i in range(expected_num_clips):
-        clip_path = os.path.join(output_dir, f"clip_{i}.mp4")
+        clip_path = os.path.join(output_dir, f"test_split_clip_{i}.mp4")
         cap = cv2.VideoCapture(clip_path)
         output_fps = cap.get(cv2.CAP_PROP_FPS)
         output_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
         clip_duration_actual = output_frame_count / output_fps
+        # round the clip duration to integer
+        clip_duration_actual = round(clip_duration_actual)
         assert clip_duration_actual == clip_duration
         cap.release()
-        
+
+
 def test_create_label_file():
     input_dir = os.path.join(os.getcwd(), "test_videos", "unlabeled_clips")
     output_file = os.path.join(os.getcwd(), "test_videos", "labels.csv")
     # Call the function to create the label file
     oneDataCollection.create_label_file(input_dir, output_file)
-    
+
     # Check if the output file exists
-    assert os.path.exists(output_file)    
-    
+    assert os.path.exists(output_file)
+
     # Read the contents of the output file
     with open(output_file, 'r') as f:
         lines = f.readlines()
-    
+
     # Check if the output file contains the expected lines
     assert len(lines) == 3
     assert lines[0].strip() == "1_good.mp4,good"
     assert lines[1].strip() == "2_bad.mp4,bad"
     assert lines[2].strip() == "3_good.mp4,good"
-    
+
+
+def test_extract_frames():
+    input_video = os.path.join(os.getcwd(), "test_videos", "test_extract.mp4")
+    output_dir = os.path.join(os.getcwd(), "test_videos", "extracted_frames")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    # Call the extract_frames function
+    oneDataCollection.extract_frames(input_video, output_dir)
+
+    # Get the frame rate of the input video
+    cap = cv2.VideoCapture(input_video)
+    # Calculate the total number of frames based on the frame rate
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+
+    # Check if the expected number of frames are extracted
+    expected_num_frames = total_frames
+    actual_num_frames = len(os.listdir(output_dir))
+    assert actual_num_frames == expected_num_frames
+
+
+def test_mediapipe_pose_estimation_frame():
+    input_folder = os.path.join(os.getcwd(), "test_videos", "extracted_frames")
+    output_folder = os.path.join(
+        os.getcwd(), "test_videos", "mediapipe_pose_estimation_frames")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Call the mediapipe_pose_estimation_frame function
+    for file in os.listdir(input_folder):
+        input_frame = os.path.join(input_folder, file)
+        output_frame_folder = os.path.join(output_folder, file.split(".")[0])
+        if not os.path.exists(output_frame_folder):
+            os.makedirs(output_frame_folder)
+        oneDataCollection.mediapipe_pose_estimation_frame(
+            input_frame, output_frame_folder)
+        assert os.path.exists(output_frame_folder)
+
+        # List the files in the output folder
+        files = os.listdir(output_frame_folder)
+        # Check if the expected number of files are created
+        assert len(files) == 2
+        # Check if the files are named correctly annotated_frame.jpg and pose_landmarks.csv
+        assert "annotated_frame.jpg" in files
+        assert "pose_landmarks.csv" in files
+
+
+def test_mediapipe_pose_estimation_frame_numpy():
+    input_folder = os.path.join(os.getcwd(), "test_videos", "extracted_frames")
+    output_folder = os.path.join(
+        os.getcwd(), "test_videos", "mediapipe_pose_estimation_frames_numpy")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Call the mediapipe_pose_estimation_frame function
+    for file in os.listdir(input_folder):
+        input_frame = os.path.join(input_folder, file)
+        output_frame_folder = os.path.join(output_folder, file.split(".")[0])
+        if not os.path.exists(output_frame_folder):
+            os.makedirs(output_frame_folder)
+        oneDataCollection.mediapipe_pose_estimation_frame_numpy(
+            input_frame, output_frame_folder)
+        assert os.path.exists(output_frame_folder)
+
+        # List the files in the output folder
+        files = os.listdir(output_frame_folder)
+        # Check if the expected number of files are created
+        assert len(files) == 4
+        # Check if the files are named correctly annotated_frame.jpg and pose_landmarks.csv
+        assert "annotated_frame.jpg" in files
+        assert "pose_landmarks.csv" in files
+        assert "pose_numpy.npy" in files
+        assert "conf_numpy.npy" in files
+
+
+def test_mediapipe_pose_estimation_consistency():
+    input_folder = os.path.join(
+        os.getcwd(), "test_videos", "mediapipe_pose_estimation_frames_numpy")
+    result = oneDataCollection.mediapipe_pose_estimation_consistency(
+        input_folder)
+    assert result == True
+
+    input_folder = os.path.join(
+        os.getcwd(), "test_videos", "mediapipe_pose_estimation_frames_numpy_fail")
+    result = oneDataCollection.mediapipe_pose_estimation_consistency(
+        input_folder)
+    assert result == False
+
+
+def test_normalize_keypoints():
+    # Test case 1: Normalizing keypoints with positive values
+    keypoints1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+    expected1 = np.array([[0., 0.09090909, 0.18181818],
+                          [0.27272727, 0.36363636,  0.45454545],
+                          [0.54545455, 0.63636364,  0.72727273],
+                          [0.81818182, 0.90909091,  1.]])
+    output = oneDataCollection.normalize_keypoints(keypoints1)
+    assert np.allclose(output, expected1)
+
+    # Test case 2: Normalizing keypoints with negative values
+    keypoints2 = np.array(
+        [[-1, -2, -3], [-4, -5, -6], [-7, -8, -9], [-10, -11, -12]])
+    expected2 = np.array(
+        [[1., 0.90909091, 0.81818182],
+         [0.72727273, 0.63636364, 0.54545455],
+         [0.45454545, 0.36363636, 0.27272727],
+         [0.18181818, 0.09090909, 0.]]
+    )
+    output = oneDataCollection.normalize_keypoints(keypoints2)
+    assert np.allclose(output, expected2)
+
+    # Test case 3: Normalizing keypoints with mixed positive and negative values
+    keypoints3 = np.array(
+        [[1, -2, 3], [-4, 5, -6], [7, -8, 9], [-10, 11, -12]])
+    expected3 = np.array(
+        [[0.56521739, 0.43478261, 0.65217391],
+         [0.34782609, 0.73913043, 0.26086957],
+         [0.82608696, 0.17391304, 0.91304348],
+         [0.08695652, 1., 0.]]
+    )
+    output = oneDataCollection.normalize_keypoints(keypoints3)
+    assert np.allclose(output, expected3)
+
+    print("All test cases passed!")
+
+def test_padding_keypoints_to_sequence():
+    frames_numpy_folder = os.path.join(
+        os.getcwd(), "test_videos", "mediapipe_pose_estimation_frames_numpy")        
+    max_sequence_length = 300
+    sequence_folder = os.path.join(os.path.dirname(frames_numpy_folder), "mediapipe_pose_estimation_sequence")
+    # Call the padding_keypoints_to_sequence function
+    oneDataCollection.padding_keypoints_to_sequence(frames_numpy_folder, max_sequence_length)
+    assert len(os.listdir(sequence_folder)) > 0
